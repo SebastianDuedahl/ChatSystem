c89f88d4181dfa77b1a5d314ab6acde4a338b176
Simon <simo390m@stud.kea.dk>
2018-10-07 13:57:14 +0200

    commit with refined chatserver

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -6,32 +6,48 @@ import java.util.ArrayList;
 public class ChatServer
 {
     int portNumber = 3333;
-
     ArrayList<ClientThread> clients = new ArrayList<>();
 
     public static void main(String[] args)
     {
         ChatServer chatServer = new ChatServer();
+        chatServer.startServer();
 
+    }
+
+    public ArrayList<ClientThread> getClients()
+    {
+        return clients;
+    }
+
+    private void startServer()
+    {
+
+        ServerSocket serverSocket = null;
         try
         {
-            ServerSocket serverSocket = new ServerSocket(chatServer.portNumber);
-            chatServer.acceptClients(serverSocket);
+            serverSocket = new ServerSocket(portNumber);
+            acceptClients(serverSocket);
         }
-        catch(IOException ioException)
+        catch (IOException ioe)
         {
-            System.out.println("Could not listen on port: " +chatServer.portNumber);
+                ioe.printStackTrace();
+            System.out.println("Could not listen on port : " + portNumber);
+
         }
+
     }
 
     private void acceptClients(ServerSocket serverSocket)
     {
+        System.out.println("Server starts port : " + serverSocket.getLocalSocketAddress());
         while(true)
         {
             try
             {
                 Socket socket = serverSocket.accept();
-                ClientThread client = new ClientThread(socket);
+                System.out.println("Accepts : " + socket.getRemoteSocketAddress());
+                ClientThread client = new ClientThread(this, socket);
                 Thread thread = new Thread (client);
                 thread.start();
                 clients.add(client);
@@ -42,11 +58,6 @@ public class ChatServer
             }
         }
 
-        
-    }
 
-    public ArrayList<ClientThread> getClients()
-    {
-        return clients;
     }
 }
